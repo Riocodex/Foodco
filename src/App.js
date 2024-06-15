543a3a131700ded5e16174b76e6089767624b3cc
Riocodex <onwukachibike@gmail.com>
2024-06-15 22:34:38 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import section2pic from './assets/eating a variety of foods-pana.png';
 import pastapic from './assets/pasta.jpeg'
 import pizzapic from './assets/pizza.jpeg'
 import shrimppic from './assets/shrimp.jpeg'
+import rosemuffin from './assets/rosemuffin.jpeg'
+import chiffonchocolate from './assets/chiffon chocolate.jpeg'
+import redvelvet from './assets/redvelvet.jpeg'
+import chocolatesmoothie from './assets/chocolatesmoothie.jpeg'
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -68,6 +72,77 @@ function App() {
           </div>
         </div>
       </div>
+
+      <div className='menu-section'>
+  <div className='menu-texts who-we-are-text'>
+    <span className='span-orange'>Menus</span>
+    <h3>Explore our Best Menu</h3>
+    <span className='span-grey'>
+      We provide the best menus on each item and even we every month <br /> 
+      provide the latest menus that can be an option for you.
+    </span>
+  </div>
+
+  <div className='menu-images who-we-are-images '>
+    <div className='menu-image who-we-are-image'>
+      <img src={rosemuffin} alt='Rose Muffin' />
+      <div className='menu-images-texts'>
+        <p>Rose Muffin</p>
+       <p><span>$45</span></p>
+      </div>
+    </div>
+    <div className='menu-image who-we-are-image'>
+      <img src={chocolatesmoothie} alt='Chocolate Smoothie' />
+      <div className='menu-images-texts'>
+        <p>Chocolate Smoothie</p>
+        <p><span>$15</span></p>
+      </div>
+    </div>
+    <div className='menu-image who-we-are-image'>
+      <img src={redvelvet} alt='Red Velvet' />
+      <div className='menu-images-texts'>
+        <p>Red Velvet</p>
+        <p><span>$25</span></p>
+      </div>
+    </div>
+    <div className='menu-image who-we-are-image'>
+      <img src={chiffonchocolate} alt='Chiffon Chocolate' />
+      <div className='menu-images-texts'>
+        <p>Chiffon Chocolate</p>
+        <p><span>$35</span></p>
+      </div>
+    </div>
+    <div className='menu-image who-we-are-image'>
+      <img src={rosemuffin} alt='Rose Muffin' />
+      <div className='menu-images-texts'>
+        <p>Rose Muffin</p>
+       <p><span>$45</span></p>
+      </div>
+    </div>
+    <div className='menu-image who-we-are-image'>
+      <img src={chocolatesmoothie} alt='Chocolate Smoothie' />
+      <div className='menu-images-texts'>
+        <p>Chocolate Smoothie</p>
+        <p><span>$15</span></p>
+      </div>
+    </div>
+    <div className='menu-image who-we-are-image'>
+      <img src={redvelvet} alt='Red Velvet' />
+      <div className='menu-images-texts'>
+        <p>Red Velvet</p>
+        <p><span>$25</span></p>
+      </div>
+    </div>
+    <div className='menu-image who-we-are-image'>
+      <img src={chiffonchocolate} alt='Chiffon Chocolate' />
+      <div className='menu-images-texts'>
+        <p>Chiffon Chocolate</p>
+        <p><span>$35</span></p>
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
   );
 }
